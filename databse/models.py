f388fdce947d9a47728b3140ce24a91fc8468ad6
Hritik Karwasra <55176834+hritikkarwasra@users.noreply.github.com>
2022-07-10 18:11:41 +0530

    Second Commit
    Updated db with all the foreign key connections

--- databse/models.py
+++ databse/models.py
@@ -70,105 +70,11 @@ Choices_AnswerFormat = {
     ('unlimited', 'unlimited'),
 }
 
-
-class test(models.Model):
-    id = models.BigIntegerField(primary_key=True, )
-    access_code = models.IntegerField()
-    who_can_initiate = models.CharField(max_length= 50, choices=Choices_WhoCanInitiate, default='bot')
-    interview_mode = models.CharField(max_length=50, choices=Choices_Interview_mode, default='audio')
-    track = models.CharField(max_length=50, choices=Choices_Track, default='slack')
-    collect_email = models.BooleanField(default=False)
-    collect_candidate_email = models.BooleanField(default=False)
-    voice_match = models.BooleanField(default=False)
-    hiring_email= models.EmailField(max_length=200)
-    collect_resume = models.BooleanField(default=False)
-    job_code = models.BigIntegerField(default=1111)
-    job_title = models.CharField(max_length=50, choices=Choices_JobTitle, default='slack')
-    job_discription = models.CharField(max_length=500)
-    total_question = models.IntegerField(max_length = 100)
-    created_at= models.DateTimeField(default=datetime.now)
-    createdby_id = models.BigIntegerField(default= 100)
-    updated_at= models.DateTimeField(default=datetime.now)
-    updatedby_id = models.BigIntegerField(default= 100)
-    is_active = models.BooleanField(default=True)
-    notification_id = models.BigIntegerField(max_length=1000)
-    company_id= models.BigIntegerField(blank=True)
-
-class companies(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name= models.CharField(max_length =100)
-    phone_number = models.CharField(max_length=12)
-    plan_id = models.BigIntegerField(max_length=20)
-    created_at= models.DateTimeField(default=datetime.now)
-    createdby_id = models.BigIntegerField(default= 100)
-    updated_at= models.DateTimeField(default=datetime.now)
-    updatedby_id = models.BigIntegerField(default= 100)
-    is_active = models.BooleanField(default=True)
-
-class Responces(models.Model):
-    id = models.IntegerField(primary_key=True)
-    response = models.CharField(max_length=500)
-    question_id = models.BigIntegerField(max_length=20) #Insert Foreignkey
-    interaction_id = models.BigIntegerField(max_length=20) #Insert Foreignkey
-    created_at= models.DateTimeField(default=datetime.now)
-    createdby_id = models.BigIntegerField(default= 100)
-    updated_at= models.DateTimeField(default=datetime.now)
-    updatedby_id = models.BigIntegerField(default= 100)
-    is_active = models.BooleanField(default=True)
-
 class Candidates(models.Model):
     id = models.BigIntegerField(primary_key=True)
     name= models.CharField(max_length =100)
     phone_number = models.CharField(max_length=12)
     email_id = models.EmailField(max_length =200, blank=True)
-    created_at= models.DateTimeField(default=datetime.now)
-    createdby_id = models.BigIntegerField(default= 100)
-    updated_at= models.DateTimeField(default=datetime.now)
-    updatedby_id = models.BigIntegerField(default= 100)
-    is_active = models.BooleanField(default=True)
-
-class Interactions(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    candidate_id = models.BigIntegerField(max_length=100) #Insert ForeignKey
-    test_id = models.BigIntegerField(max_length=100) #Insert ForeignKey
-    candidate_feedback = models.CharField(max_length=100)
-    report_sent_to_user = models.BooleanField(default=False)
-    channel = models.CharField(choices=Choices_Interview_channel,max_length=20)
-    created_at= models.DateTimeField(default=datetime.now)
-    createdby_id = models.BigIntegerField(default= 100)
-    updated_at= models.DateTimeField(default=datetime.now)
-    updatedby_id = models.BigIntegerField(default= 100)
-    is_active = models.BooleanField(default=True)
-
-class Scores(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    resume_score = models.IntegerField(max_length= 2, default=0)
-    pace = models.IntegerField(max_length= 2, default=0)
-    power_words = models.IntegerField(max_length= 2, default=0)
-    value_scale = models.IntegerField(max_length= 2, default=0)
-    pitch_range = models.IntegerField(max_length= 2, default=0)
-    gesture = models.IntegerField(max_length= 2, default=0)
-    content_score = models.IntegerField(max_length= 2, default=0)
-    overall = models.IntegerField(max_length= 2, default=0)
-    
-class Score_per_question(models.Model):
-    id = models.BigIntegerField(primary_key=True,)
-    score_id = models.BigIntegerField(max_length=10) #insert Foreignkey
-    question_id = models.BigIntegerField(max_length=10) #insert Foreignkey
-    likebality = models.IntegerField(max_length= 2, default=0)
-    charm = models.IntegerField(max_length= 2, default=0)
-    confidence = models.IntegerField(max_length= 2, default=0)
-    fluency = models.IntegerField(max_length= 2, default=0)
-    content = models.IntegerField(max_length= 2, default=0)
-    overall = models.IntegerField(max_length= 2, default=0)
-
-class Questions(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    question = models.CharField(max_length= 500)
-    answer_format = models.CharField(max_length=50, choices=Choices_AnswerFormat)
-    ideal_answer = models.CharField(max_length= 500)
-    should_rate = models.BooleanField(default=True)
-    test_id = models.BigIntegerField(max_length=10)
     created_at= models.DateTimeField(default=datetime.now)
     createdby_id = models.BigIntegerField(default= 100)
     updated_at= models.DateTimeField(default=datetime.now)
@@ -188,6 +94,19 @@ class Plan(models.Model):
     updatedby_id = models.BigIntegerField(default= 100)
     is_active = models.BooleanField(default=True)
 
+
+class Companies(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name= models.CharField(max_length =100)
+    phone_number = models.CharField(max_length=12)
+    plan_id = models.ForeignKey(Plan, on_delete=models.PROTECT,blank=True)
+    created_at= models.DateTimeField(default=datetime.now)
+    createdby_id = models.BigIntegerField(default= 100)
+    updated_at= models.DateTimeField(default=datetime.now)
+    updatedby_id = models.BigIntegerField(default= 100)
+    is_active = models.BooleanField(default=True)
+
+
 class Notifications(models.Model):
     id = models.BigIntegerField(max_length=10)
     interaction_welcome = models.CharField(max_length= 500)
@@ -199,6 +118,20 @@ class Notifications(models.Model):
     updated_at= models.DateTimeField(default=datetime.now)
     updatedby_id = models.BigIntegerField(default= 100)
     is_active = models.BooleanField(default=True)
+
+
+class Scores(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    resume_score = models.IntegerField(max_length= 2, default=0)
+    pace = models.IntegerField(max_length= 2, default=0)
+    power_words = models.IntegerField(max_length= 2, default=0)
+    value_scale = models.IntegerField(max_length= 2, default=0)
+    pitch_range = models.IntegerField(max_length= 2, default=0)
+    gesture = models.IntegerField(max_length= 2, default=0)
+    content_score = models.IntegerField(max_length= 2, default=0)
+    overall = models.IntegerField(max_length= 2, default=0)
+
+
 
 class Indivdual_reports(models.Model):
     id = models.BigIntegerField(primary_key=True)
@@ -220,7 +153,92 @@ class LeaderBoard_report(models.Model):
     initial_box = models.CharField(max_length=100)
     letter_text = models.CharField(max_length=100)
     rating_variable = models.CharField(max_length=100)
+     
+
+################################################################
+
+
+
+class Test(models.Model):
+    id = models.BigIntegerField(primary_key=True, )
+    company_id= models.ForeignKey(Companies, on_delete=models.PROTECT,blank=True)
+    access_code = models.IntegerField()
+    who_can_initiate = models.CharField(max_length= 50, choices=Choices_WhoCanInitiate, default='bot')
+    interview_mode = models.CharField(max_length=50, choices=Choices_Interview_mode, default='audio')
+    track = models.CharField(max_length=50, choices=Choices_Track, default='slack')
+    collect_email = models.BooleanField(default=False)
+    collect_candidate_email = models.BooleanField(default=False)
+    voice_match = models.BooleanField(default=False)
+    hiring_email= models.EmailField(max_length=200)
+    collect_resume = models.BooleanField(default=False)
+    job_code = models.BigIntegerField(default=1111)
+    job_title = models.CharField(max_length=50, choices=Choices_JobTitle, default='slack')
+    job_discription = models.CharField(max_length=500)
+    total_question = models.IntegerField(max_length = 100)
+    created_at= models.DateTimeField(default=datetime.now)
+    createdby_id = models.BigIntegerField(default= 100)
+    updated_at= models.DateTimeField(default=datetime.now)
+    updatedby_id = models.BigIntegerField(default= 100)
+    is_active = models.BooleanField(default=True)
+    notification_id = models.ForeignKey(Notifications, on_delete=models.PROTECT,blank=True)
+
+class Questions(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    question = models.CharField(max_length= 500)
+    answer_format = models.CharField(max_length=50, choices=Choices_AnswerFormat)
+    ideal_answer = models.CharField(max_length= 500)
+    should_rate = models.BooleanField(default=True)
+    test_id= models.ForeignKey(Test, on_delete=models.PROTECT,blank=True)
+    created_at= models.DateTimeField(default=datetime.now)
+    createdby_id = models.BigIntegerField(default= 100)
+    updated_at= models.DateTimeField(default=datetime.now)
+    updatedby_id = models.BigIntegerField(default= 100)
+    is_active = models.BooleanField(default=True)
+
+
+class Interactions(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    candidate_id = models.ForeignKey(Candidates,on_delete=models.PROTECT,blank=True) #Insert Foreignkey
+    test_id = models.ForeignKey(Test,on_delete=models.PROTECT,blank = True ) #Insert ForeignKey
+    candidate_feedback = models.CharField(max_length=100)
+    report_sent_to_user = models.BooleanField(default=False)
+    channel = models.CharField(choices=Choices_Interview_channel,max_length=20)
+    created_at= models.DateTimeField(default=datetime.now)
+    createdby_id = models.BigIntegerField(default= 100)
+    updated_at= models.DateTimeField(default=datetime.now)
+    updatedby_id = models.BigIntegerField(default= 100)
+    score_id = models.ForeignKey(Scores,on_delete=models.PROTECT,blank = True ) #Insert ForeignKey
+    individual_report_id = models.ForeignKey(Indivdual_reports,on_delete=models.PROTECT,blank = True ) #Insert ForeignKey
+
+    is_active = models.BooleanField(default=True)
     
+class Responces(models.Model):
+    id = models.IntegerField(primary_key=True)
+    interaction_id = models.ForeignKey(Interactions,on_delete=models.PROTECT,blank=True) #Insert Foreignkey
+    question_id = models.ForeignKey(Questions,on_delete=models.PROTECT,blank=True) #Insert Foreignkey
+    candidate_id = models.ForeignKey(Candidates,on_delete=models.PROTECT,blank=True) #Insert Foreignkey
+    response = models.CharField(max_length=500)
+    created_at= models.DateTimeField(default=datetime.now)
+    createdby_id = models.BigIntegerField(default= 100)
+    updated_at= models.DateTimeField(default=datetime.now)
+    updatedby_id = models.BigIntegerField(default= 100)
+    is_active = models.BooleanField(default=True)
+
+
+
+
+class Score_per_question(models.Model):
+    id = models.BigIntegerField(primary_key=True,)
+    score_id = models.ForeignKey(Scores,on_delete=models.PROTECT,blank = True ) #Insert ForeignKey
+    question_id = models.ForeignKey(Questions,on_delete=models.PROTECT,blank = True ) #Insert ForeignKey
+    likebality = models.IntegerField(max_length= 2, default=0)
+    charm = models.IntegerField(max_length= 2, default=0)
+    confidence = models.IntegerField(max_length= 2, default=0)
+    fluency = models.IntegerField(max_length= 2, default=0)
+    content = models.IntegerField(max_length= 2, default=0)
+    overall = models.IntegerField(max_length= 2, default=0)
+
+
 
 
 
