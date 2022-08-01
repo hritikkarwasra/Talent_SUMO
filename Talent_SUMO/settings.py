8b66f3b8bce8480ecf43f8bfa8f23edffe471a15
Hritik Karwasra <55176834+hritikkarwasra@users.noreply.github.com>
2022-08-01 17:07:22 +0530

    Update2

--- Talent_SUMO/settings.py
+++ Talent_SUMO/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -36,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'databse'
 ]
 
